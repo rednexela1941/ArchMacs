3c5a10070e438f00dc3e95bb23ca97dd14d09fc7
Alex <40923759+rednexela1941@users.noreply.github.com>
2020-09-04 23:46:05 -0400

    init file

--- init.el
+++ init.el
@@ -46,7 +46,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(gruvbox-theme c-eldoc lsp-mode json-mode yapfify js2-mode tern scss-mode haskell-mode company-mode company-web web-mode tide ## web-beautify typescript-mode doom-themes)))
+   '(go-mode gruvbox-theme c-eldoc lsp-mode json-mode yapfify js2-mode tern scss-mode haskell-mode company-mode company-web web-mode tide ## web-beautify typescript-mode doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -78,6 +78,7 @@
   (web-mode)
   (company-mode)
   (tide-mode)
+  (flycheck-mode)
 )
 
 
