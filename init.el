edea71d082fc088600f6ba86a6fe387617156328
at <40923759+rednexela1941@users.noreply.github.com>
2021-02-06 13:09:56 -0500

    tabify

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-	 '(prettier-js neotree s "s" racer toml-mode flycheck-rust monokai-theme sublime-themes nasm-mode slime-company slime rust-mode clang-format magit multiple-cursors company-go go-autocomplete go-complete exec-path-from-shell julia-mode go-eldoc humanoid-themes go-mode gruvbox-theme c-eldoc lsp-mode json-mode yapfify tern scss-mode haskell-mode company-mode company-web web-mode tide ## web-beautify typescript-mode doom-themes)))
+	 '(dockerfile-mode go-playground prettier-js neotree s "s" racer toml-mode flycheck-rust monokai-theme sublime-themes nasm-mode slime-company slime rust-mode clang-format magit multiple-cursors company-go go-autocomplete go-complete exec-path-from-shell julia-mode go-eldoc humanoid-themes go-mode gruvbox-theme c-eldoc lsp-mode json-mode yapfify tern scss-mode haskell-mode company-mode company-web web-mode tide ## web-beautify typescript-mode doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -26,6 +26,7 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+(setq-default indent-tabs-mode t)
 (set-frame-font "Source Code Pro 8")
 (add-to-list 'default-frame-alist '(font . "Source Code Pro 8"))
 (setq column-number-mode 1)
@@ -33,11 +34,11 @@
 (setq-default tab-width 2)
 (setq create-lockfiles nil)
 (setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
+			`((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+			`((".*" ,temporary-file-directory t)))
 (when (version<= "26.0.50" emacs-version )
-  (global-display-line-numbers-mode))
+	(global-display-line-numbers-mode))
 (setq x-super-keysym 'meta)
 (global-unset-key (kbd "C-z"))
 (global-set-key (kbd "C-x g") 'magit-status)
@@ -45,7 +46,7 @@
 ;;-------Themes-------
 (if (display-graphic-p)
 	(load-theme 'doom-one t)
-  (load-theme 'monokai t)
+	(load-theme 'monokai t)
  )
 ;;https://github.com/hlissner/emacs-doom-themes -- Doom Themes
 ;;https://github.com/greduan/emacs-theme-gruvbox -- Gruvbox Themes
@@ -53,9 +54,9 @@
 ;;(load-theme 'doom-one t)
 ;;(load-theme 'doom-vibrant t)
 ;; (if (display-graphic-p)
-;; 		(load-theme 'humanoid-dark t)
-;;   (load-theme 'monokai t)
-;;  )
+;;			(load-theme 'humanoid-dark t)
+;;	 (load-theme 'monokai t)
+;;	)
 ;;(load-theme 'doom-outrun-electric t)
 ;;(load-theme 'gruvbox-dark-medium t)
 ;;(load-theme 'humanoid-dark t)
@@ -66,8 +67,8 @@
 ;;(load-theme 'wilson t)
 
 (global-set-key (kbd "<f6>")
-  (lambda() (interactive) (find-file "~/.emacs.d/init.el")
-    ))
+	(lambda() (interactive) (find-file "~/.emacs.d/init.el")
+		))
 ;; neotree
 (global-set-key [f8] 'neotree-toggle)
 
@@ -92,122 +93,123 @@
 
 ;; Multiple cursor setiup.
 (defun start-multiple-cursors()
-  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-  )
+	(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+	(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+	(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+	)
 
 (start-multiple-cursors)
 
 ;; Perl Formatter: Requires https://github.com/perltidy/perltidy
 (defun perltidy ()
-  (interactive) 
-  (let ((doc (buffer-string)) (tmpfile (concat "/tmp/emacs_perltidy_" (buffer-name) (s-replace ":" "" (s-replace " " "" (current-time-string))))))
+	(interactive) 
+	(let ((doc (buffer-string)) (tmpfile (concat "/tmp/emacs_perltidy_" (buffer-name) (s-replace ":" "" (s-replace " " "" (current-time-string))))))
 	(progn 
-	  (with-temp-file tmpfile
+		(with-temp-file tmpfile
 		(progn 
-		  (write-region doc nil tmpfile)
-		  (shell-command (concat "perltidy -ce " tmpfile))
-		  )  
+			(write-region doc nil tmpfile)
+			(shell-command (concat "perltidy -ce " tmpfile))
+			)	 
 		)
-	  (insert-file-contents (concat tmpfile ".tdy") nil nil nil t)
-	  (shell-command (concat "rm "(concat tmpfile "*")))
-	  )
+		(insert-file-contents (concat tmpfile ".tdy") nil nil nil t)
+		(shell-command (concat "rm "(concat tmpfile "*")))
+		)
 	)
-  )
+	)
 
 (defun setup-perl-mode()
-  (interactive)
-  (perl-mode)
-  (company-mode)
-  (flycheck-mode)
-  (add-hook 'before-save-hook #'perltidy 0 'local)
-  )
+	(interactive)
+	(perl-mode)
+	(company-mode)
+	(flycheck-mode)
+	(add-hook 'before-save-hook #'perltidy 0 'local)
+	)
 
 (defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (tide-hl-identifier-mode +1)
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
-  (company-mode )
-  (web-mode)
-  (eldoc-mode )
+	(interactive)
+	(tide-setup)
+	(flycheck-mode +1)
+	(setq flycheck-check-syntax-automatically '(save mode-enabled))
+	(tide-hl-identifier-mode +1)
+	;; company is an optional dependency. You have to
+	;; install it separately via package-install
+	;; `M-x package-install [ret] company`
+	(company-mode )
+	(web-mode)
+	(eldoc-mode )
 )
 
 (defun tsx-mode ()
-  (interactive)
-  (web-mode)
-  (company-mode)
-  (tide-mode)
-  (flycheck-mode)
-  (tide-restart-server)
+	(interactive)
+	(web-mode)
+	(company-mode)
+	(tide-mode)
+	(prettier-js-mode)
+	(flycheck-mode)
+	(tide-restart-server)
 )
 
 (defun setup-css-mode ()
-  (interactive)
-  (scss-mode)
-  (company-mode)
-  (message "setup-css-mode")
-  )
+	(interactive)
+	(scss-mode)
+	(company-mode)
+	(message "setup-css-mode")
+	)
 
 (defun setup-ts-mode ()
-  (interactive)
-  (typescript-mode)
-  (company-mode)
-  (tide-setup)
-  (flycheck-mode)
-  (message "setup-ts-mode")
-  )
+	(interactive)
+	(typescript-mode)
+	(company-mode)
+	(tide-setup)
+	(flycheck-mode)
+	(message "setup-ts-mode")
+	)
 
-(setq prettier-js-args '(	"--single-quote" "--tab-width" "4" ))
+(setq prettier-js-args '(	"--single-quote" "--use-tabs" ))
 
 (defun setup-javascript-mode ()
-  (interactive)
-  (company-mode)
-  (flycheck-mode)
-  (javascript-mode)
+	(interactive)
+	(company-mode)
+	(flycheck-mode)
+	(javascript-mode)
 	(prettier-js-mode)
-  (message "setup-javascript-mode")
-  )
+	(message "setup-javascript-mode")
+	)
 
 (defun setup-c-mode ()
-  (interactive)
-  (c-mode)
-  (company-mode)
-  (c-turn-on-eldoc-mode)
-  (message "setup-c-mode")
-  )
+	(interactive)
+	(c-mode)
+	(company-mode)
+	(c-turn-on-eldoc-mode)
+	(message "setup-c-mode")
+	)
 
 (defun setup-go-mode ()
-  (interactive)
-  (go-mode)
-  ((lambda ()
-    (set (make-local-variable 'company-backends) '(company-go))
-    (company-mode)))
-  (company-mode)
-  (flycheck-mode)
-  (message "setup-go-mode")
-  )
+	(interactive)
+	(go-mode)
+	((lambda ()
+		(set (make-local-variable 'company-backends) '(company-go))
+		(company-mode)))
+	(company-mode)
+	(flycheck-mode)
+	(message "setup-go-mode")
+	)
 
 (defun setup-julia-mode ()
-  (interactive)
-  (julia-mode)
-  (company-mode)
-  (message "setup-julia-mode")
-  )
+	(interactive)
+	(julia-mode)
+	(company-mode)
+	(message "setup-julia-mode")
+	)
 
 (defun setup-python-mode ()
-  (interactive)
-  (python-mode)
-  ;; (add-to-list 'company-backends 'company-jedi)
-;;   (company-mode)
-;;  (yapf-mode)
-  (message "setup-python-mode")
-  )
+	(interactive)
+	(python-mode)
+	;; (add-to-list 'company-backends 'company-jedi)
+;;	 (company-mode)
+;;	(yapf-mode)
+	(message "setup-python-mode")
+	)
 
 (add-to-list 'auto-mode-alist '("\\.pl\\'" . setup-perl-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-mode))
@@ -225,18 +227,18 @@
 (setq company-tooltip-align-annotations t)
 
 (defun save-scss ()
-  (when (eq major-mode 'scss-mode)
-    (web-beautify-css))
-  )
+	(when (eq major-mode 'scss-mode)
+		(web-beautify-css))
+	)
 
 (defun save-javascript ()
-  (when (eq major-mode 'javascript-mode)
-    (web-beautify-js))
-  )
+	(when (eq major-mode 'javascript-mode)
+		(web-beautify-js))
+	)
 
 (add-hook 'before-save-hook #'save-scss)
-(add-hook 'before-save-hook #'save-javascript)
-(add-hook 'before-save-hook 'tide-format-before-save)
+;; (add-hook 'before-save-hook #'save-javascript)
+;;(add-hook 'before-save-hook 'tide-format-before-save)
 
 (setq company-dabbrev-downcase 0)
 (setq company-idle-delay 0)
@@ -249,34 +251,34 @@
 (setq rust-format-on-save t)
 
 (setq c-default-style
-      '((java-mode . "java")
-        (awk-mode . "awk")
+			'((java-mode . "java")
+				(awk-mode . "awk")
 				(c-mode . "k&r")
-        (other . "gnu")))
+				(other . "gnu")))
 
 ;; tag a line for jumping.
 (defun jmp-tag-line () 
-  (interactive)
-  (setq jmp-tag (line-number-at-pos))
-  (message (concat "Set jmp-tag to line: " (number-to-string jmp-tag)))
-  )
+	(interactive)
+	(setq jmp-tag (line-number-at-pos))
+	(message (concat "Set jmp-tag to line: " (number-to-string jmp-tag)))
+	)
 
 ;; Return to position jmped from.
 (defun jmp-back ()
-  (interactive)
-  (if last-jmp-tag 
-	  (progn
+	(interactive)
+	(if last-jmp-tag 
+		(progn
 		(goto-line last-jmp-tag)
 		(message (concat "Jumped back to line: " (number-to-string last-jmp-tag))))
 	(message "No last-jmp-tag found.")
 	)
-  )
+	)
 
 ;; jmp to tag.
 (defun jmp-to-tag ()
-  (interactive)
-  (if jmp-tag
-	  (progn
+	(interactive)
+	(if jmp-tag
+		(progn
 		(setq last-jmp-tag (line-number-at-pos))
 		(goto-line jmp-tag)
 		(message (concat "Jumped to line: " (number-to-string jmp-tag))))
@@ -284,34 +286,34 @@
 	))
 
 (defun jmp-start ()
-  (interactive)
-  (progn
+	(interactive)
+	(progn
 	(jmp-tag-line)
 	(beginning-of-buffer)
 	(message (concat "Set jmp-tag to line: " (number-to-string jmp-tag))
 			 )))
 
 (defun jmp-end ()
-  (interactive)
-  (progn
+	(interactive)
+	(progn
 	(jmp-tag-line)
 	(end-of-buffer)
 	(message (concat "Set jmp-tag to line: " (number-to-string jmp-tag))
 			 )))
 
 (defun replace-region-matches ()
-  (interactive)
-  (if (region-active-p)
-	  (let ((startpos (region-beginning)) (qstring (buffer-substring (region-beginning) (region-end))))		
+	(interactive)
+	(if (region-active-p)
+		(let ((startpos (region-beginning)) (qstring (buffer-substring (region-beginning) (region-end))))
 		(let ((rstring (read-string (concat "Replace " qstring ": "))))
-		  (progn 
+			(progn
 			(replace-string qstring rstring nil (point-min) (point-max))
 			(goto-char startpos)
 			(message "Replaced %s with %s in buffer." qstring rstring)
-		  )))
+			)))
 	(message "No region defined.")
 	)
-  )
+	)
 
 
 (global-set-key (kbd "C-c C-r") 'replace-region-matches)
